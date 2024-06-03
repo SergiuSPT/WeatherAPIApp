1c266ec508fb8362fa59e373e15fbca44280d403
SergiuSPT <96991196+SergiuSPT@users.noreply.github.com>
2024-06-03 11:24:47 +0300

    initial front-end

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 const API_URL = "http://api.weatherapi.com/v1";
+const key = "5f857c804e30492a873103739243005";
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
