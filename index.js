48729285520cf145aaeed368e8f5bcab7d5e706a
SergiuSPT <96991196+SergiuSPT@users.noreply.github.com>
2024-06-07 10:57:51 +0300

    start implement details section

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const key = "5f857c804e30492a873103739243005";
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 let h1var;
+let detailsFlag = 0;
+let location;
+let days;
 app.get("/", (req, res) => {
     h1var = "Insert the city you want to see the weather for and for how many days...";
     res.render("index.ejs", {
@@ -20,8 +23,8 @@ app.get("/", (req, res) => {
 app.post("/submit", async (req, res) => {
     try{
         h1var = "Here is your forecast";
-        let location = req.body["location"];
-        let days = req.body["days"];
+        location = req.body["location"];
+        days = req.body["days"];
         
         const response = await axios.get(API_URL + `?key=${key}&q=${location}&days=${days}`);
         let result = response.data;
@@ -38,7 +41,28 @@ app.post("/submit", async (req, res) => {
             error:"No activities that match your criteria.",
         });
     }
-    
+
+});
+
+app.post("/details" , async (req, res) =>{
+    try{
+        detailsFlag = 1;
+        h1var = "Here is your forecast";
+        const response = await axios.get(API_URL + `?key=${key}&q=${location}&days=${days}`);
+        let result = response.data;
+        console.log(result);
+        res.render("index.ejs", {
+            h1: h1var,
+            location: location,
+            days: days,
+            weather: result,
+        });
+    }catch(error){
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs",{
+            error:"No activities that match your criteria.",
+        });
+    }
 });
 
 app.listen(port, () => {
